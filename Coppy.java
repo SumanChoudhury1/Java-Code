c931471dacb8bb27e00d3426fc0ecaed4a838a5c
SumanChoudhury1 <sc850016@gmail.com>
2024-07-02 13:35:03 +0530

    java

---
+++ Coppy.java
@@ -0,0 +1,21 @@
+class Coppy {
+
+    String brand;
+    String name;
+    int price;
+
+
+    public static Void Main(string[]args)
+    {
+        Coppy c1=new Coppy();
+        c1 brand="samsume";
+        c1 name="nokia";
+        c1 price=1400;
+
+        system.out.println(c1.brand);
+        system.out.println(c1.name);
+        system.out.print(c1.price);
+
+
+    }
+}
